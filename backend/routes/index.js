5144a5684b930eb9cb1e5d400ddc747a5f588d20
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-09-13 16:46:45 +0900

    добавить роут для краш теста сервера

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -14,6 +14,12 @@ const { login, postUser } = require('../controllers/users');
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
